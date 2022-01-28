4a516833f3eae541e7a938b109b91512bd9d24d8
Suranjana30 <90706866+Suranjana30@users.noreply.github.com>
2022-01-28 14:11:37 +0530

    Add files via upload

---
+++ Assignment 3a.js
@@ -0,0 +1,8 @@
+const input=prompt("Enter Your Name: ");
+console.log("Hello"+" "+input);
+const email=prompt("Enter Your Email-ID:");
+const password=prompt("Enter your Password: ");
+(email=="stu@letsupgrade.com" && password == "12345")
+?alert("Hurray!!!You are Logged in")
+:alert("Please try again with correct Email-ID and Password");
+
